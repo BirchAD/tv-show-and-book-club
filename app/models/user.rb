288f29e32637c30b483be9e069193236b080efa9
Andrew Birch <andrewbirch1882@icloud.com>
2022-12-24 21:36:59 +0000

    User model TDD to return initialised name, checking if user has multiple names separated  with whitespace or -

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}"
+  end
+
+  def initials
+    first_names = first_name.split(/[\s,-]+/).map { |name| name[0] }.join.upcase
+    last_names = last_name.split(/[\s,-]+/).map { |name| name[0] }.join.upcase
+    "#{first_names}#{last_names}"
+  end
 end
