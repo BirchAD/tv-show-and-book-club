b420e156d56602381cb0060efb2a56a8b70155d6
Andrew Birch <andrewbirch1882@icloud.com>
2022-12-26 18:03:34 +0000

    TESTS and VALIDATIONS: First name validation TDD

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, length: { in: 3..10 }
+  validates :first_name,
+            presence: true,
+            format: { with: /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/,
+                      message: 'only allows letters and one space after a letter' },
+            length: { maximum: 25 }
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
   end
--- test/models/UserModelTests/username_validation_test.rb
+++ test/models/UserModelTests/username_validation_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
   def setup
     @user = users(:valid_user)
   end
@@ -19,12 +18,12 @@ class UserTest < ActiveSupport::TestCase
   test "username should not exceed 10 characters" do
     @user.username = "p" * 15
     refute @user.valid?, 'Username valid if it exceeds 10 characters'
-    assert_not_equal @user.errors[:username], 'no validation error for username exceeding 10 chars'
+    assert_not_nil @user.errors[:username], 'no validation error for username exceeding 10 chars'
   end
 
   test "username should not be less than 3 characters" do
     @user.username = "pp"
     refute @user.valid?, 'Username valid if it is less than 3 characters'
-    assert_not_equal @user.errors[:username], 'no validation error for username less than 3 chars'
+    assert_not_nil @user.errors[:username], 'no validation error for username less than 3 chars'
   end
 end
