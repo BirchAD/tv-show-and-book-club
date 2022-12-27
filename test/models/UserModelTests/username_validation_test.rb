e7dba9048bb4c622eda99b7004c7d3424e353026
Andrew Birch <andrewbirch1882@icloud.com>
2022-12-27 13:42:17 +0000

    BUGFIX : Fixed bug with duplicate test names; REFACTOR: refute to assert_not

--- test/models/UserModelTests/username_validation_test.rb
+++ test/models/UserModelTests/username_validation_test.rb
@@ -6,37 +6,37 @@ class UserTest < ActiveSupport::TestCase
     @same_username = users(:same_username)
   end
 
-  test 'user should be valid' do
+  test 'username user test should be valid' do
     assert @user.valid?
   end
 
   test 'invalid without username' do
     @user.username = ""
-    refute @user.valid?, 'Username valid is username is empty'
+    assert_not @user.valid?, 'Username valid is username is empty'
     assert_not_nil @user.errors[:username], 'no validation error for username present'
   end
 
   test "username should not exceed 10 characters" do
     @user.username = "p" * 15
-    refute @user.valid?, 'Username valid if it exceeds 10 characters'
+    assert_not @user.valid?, 'Username valid if it exceeds 10 characters'
     assert_not_nil @user.errors[:username], 'no validation error for username exceeding 10 chars'
   end
 
   test "username should not be less than 3 characters" do
     @user.username = "pp"
-    refute @user.valid?, 'Username valid if it is less than 3 characters'
+    assert_not @user.valid?, 'Username valid if it is less than 3 characters'
     assert_not_nil @user.errors[:username], 'no validation error for username less than 3 chars'
   end
 
   test 'username must be unique string' do
     @same_username.username = @user.username
-    refute @same_username.valid?, 'Username valid if not unique string'
+    assert_not @same_username.valid?, 'Username valid if not unique string'
     assert_not_nil @same_username[:username], 'no validation for unique username'
   end
 
   test 'username must be unique based on case sensitivity' do
     @same_username.username = "johnd"
-    refute @same_username.valid?, 'Username valid if not case insensitive'
+    assert_not @same_username.valid?, 'Username valid if not case insensitive'
     assert_not_nil @same_username[:username], 'no validation for unique username based on case sens'
   end
 end
