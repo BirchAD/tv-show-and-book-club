76158e20d46d06154d27239cf42dbfe29b7b22d3
Andrew Birch <andrewbirch1882@icloud.com>
2023-01-10 17:36:42 +0000

    TDD for user authorization.  User redirected to their own profile page if attempting to view another users

--- test/integration/user_profile_test.rb
+++ test/integration/user_profile_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UserProfileTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:valid_user)
+    @second_user = users(:another_user)
   end
 
   test 'user can view profile page' do
@@ -17,4 +18,18 @@ class UserProfileTest < ActionDispatch::IntegrationTest
     assert_routing '/profile', { controller: 'users', action: 'show' }
   end
   Rails.application.routes.recognize_path '/profile', method: :get
+
+  test 'not logged in user is redirected to login page when attempting to view a profile' do
+    get profile_path
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test 'user cannot view the profile of another user' do
+    login_as @user
+    get profile_path(@second_user)
+    assert_response :redirect
+    assert_redirected_to profile_path
+    assert_equal flash[:alert], "You can only view your profile"
+  end
 end
