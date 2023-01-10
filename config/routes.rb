6fab9040d2db89dd15f578e7f08c5a40239fc464
Andrew Birch <andrewbirch1882@icloud.com>
2023-01-10 13:33:31 +0000

    User profile page TDD and basic functionality of show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :clubs, only: %i[new create show index] do
     resources :memberships, only: %i[new create]
   end
+  get "/profile", to: "users#show"
 end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  # def test_truth
+  #   assert true
+  # end
+end
